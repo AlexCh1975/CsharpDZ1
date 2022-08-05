dfba18c8e6c8ff1376318e8f2f2b0a32a1e1ddce
AlexCh <stovertikal@mail.ru>
2022-08-05 20:19:16 +0700

    add task 008

---
+++ task_008/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Задача 8: Напишите программу, которая на вход принимает число (N), 
+    а на выходе показывает все чётные числа от 1 до N.
+    5 -> 2, 4
+    8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+
+void EvenNumber(int N)
+{
+    Console.Write($"Четные числа от 0 до {N} -> ");
+
+    int count = 2;
+    while (count <= N)
+    {
+        if (count % 2 == 0) 
+        {
+            Console.Write(count);
+            Console.Write(" ");
+        }
+        count++;
+    }
+    
+    Console.WriteLine();
+}
+
+EvenNumber(5);
+EvenNumber(8);
+
