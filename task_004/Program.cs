a5da584c35ec4855465471096d91b7dc918d3d20
AlexCh <stovertikal@mail.ru>
2022-08-05 18:21:50 +0700

    add task 004

---
+++ task_004/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Задача 4: Напишите программу, которая принимает на вход 
+    три числа и выдаёт максимальное из этих чисел.
+    2, 3, 7 -> 7
+    44 5 78 -> 78
+    22 3 9 -> 22
+*/
+ Console.Clear();
+
+ void MaxNumber(int a, int b, int c)
+ {
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+
+    Console.Write(a);
+    Console.Write(", ");
+    Console.Write(b);
+    Console.Write(", ");
+    Console.Write(c);
+    Console.Write("  -> ");
+    Console.Write("max = ");
+    Console.WriteLine(max);
+ }
+
+MaxNumber(2, 3, 7);
+MaxNumber(44, 5, 78);
+MaxNumber(22, 3, 9);
+
+ 
