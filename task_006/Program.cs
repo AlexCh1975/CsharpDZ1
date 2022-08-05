8cb534147aa04c9180a006e153e0500473c6c5ae
AlexCh <stovertikal@mail.ru>
2022-08-05 19:23:10 +0700

    add task 006

---
+++ task_006/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Задача 6: Напишите программу, которая на вход принимает 
+    число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+    4 -> да
+    -3 -> нет
+    7 -> нет
+*/
+
+Console.Clear();
+
+Console.WriteLine("Является ли число чётным?");
+void EvenNumber(int num)
+{ 
+    if (num % 2 == 0)
+    {
+        Console.WriteLine($"{num} -> да");
+    }
+    else 
+    {
+        Console.WriteLine($"{num} -> нет");
+    }
+}
+
+EvenNumber(4);
+EvenNumber(-3);
+EvenNumber(7);
